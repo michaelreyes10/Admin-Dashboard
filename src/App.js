b8309264b51f91e607a9f8f72f4ca052caafbb3e
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-12-03 19:33:51 -0500

    grab img file

--- src/App.js
+++ src/App.js
@@ -4,27 +4,31 @@ import List from "./pages/list/List";
 import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Single from "./pages/single/Single";
+import { productInputs, userInputs } from "./formSource";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={ <Home/> } />
-            <Route path="login" element={ <Login /> } />
-            
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
             <Route path="users">
-              <Route index element={ <List /> } />
-              <Route path=":userId" element={<Single/>} />
-              <Route path="new" element={ <New /> } />
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
             </Route>
-
             <Route path="products">
-              <Route index element={ <List /> } />
-              <Route path=":productId" element={<Single/>} />
-              <Route path="new" element={ <New /> } />
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
             </Route>
-
           </Route>
         </Routes>
       </BrowserRouter>
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -3,15 +3,20 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import "./new.scss";
 import { DriveEtaRounded } from "@mui/icons-material";
+import { useState } from "react";
 
-const New = () => {
+const New = ({inputs, title}) => {
+
+    const [file, setFile] = useState("");
+
+    console.log(file)
     return(
         <div className="new">
             <Sidebar/>
             <div className="newContainer">
                 <Navbar/>
                 <div className="top">
-                    <h1>Add New User</h1>
+                    <h1>{title}</h1>
                 </div>
                 <div className="bottom">
                     <div className="left">
@@ -23,44 +28,22 @@ const New = () => {
                                 <label htmlFor="file">
                                   Image: <DriveFolderUploadOutlinedIcon className="icon"/>
                                 </label>
-                                <input type="file" id="file" style={{display: "none"}} />
-                                
-                            </div>
-                            <div className="formInput">
-                                <label>Username</label>
-                                <input type='text' placeholder='john_doe'></input>
-                            </div>
-                            <div className="formInput">
-                                <label>Full Name</label>
-                                <input type='text' placeholder='john_doe'></input>
-                            </div>
-                            <div className="formInput">
-                                <label>Email</label>
-                                <input type='text' placeholder='john_doe@gmail.com'></input>
-                            </div>
-                            <div className="formInput">
-                                <label>Phone</label>
-                                <input type='text' placeholder='+1 234 567 89'></input>
-                            </div>
-                            <div className="formInput">
-                                <label>Password</label>
-                                <input type='text'></input>
-                            </div>
-                            <div className="formInput">
-                                <label>Address</label>
-                                <input type='text' placeholder="Elton St. 216 NewYork"></input>
-                            </div>
-                            <div className="formInput">
-                                <label>Country</label>
-                                <input type='text' placeholder="USA"></input>
-                            </div>
-                            <button>Submit</button>
-                        </form>
-                    </div>
+                                <input type="file" id="file" onChange={e=>setFile(e.target.files[0])}style={{display: "none"}} />
+                             </div>
+                             
+                             {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
                 </div>
-            </div>
+              ))}
+              <button>Send</button>
+            </form>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default New
